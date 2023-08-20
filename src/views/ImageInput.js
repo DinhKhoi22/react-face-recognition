9bce3de99b9abc59ee1b35f806cb78b5f27abb0c
DinhKhoi22 <111755052+DinhKhoi22@users.noreply.github.com>
2023-08-20 11:55:37 +0700

    React-face-recognition

--- src/views/ImageInput.js
+++ src/views/ImageInput.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { loadModels, getFullFaceDescription, createMatcher } from '../api/face';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { loadModels, getFullFaceDescription, createMatcher } from "../api/face";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 // Import image to test API
-const testImg = require('../img/test.jpg');
+const warnImg = require("../img/test.jpg");
 
 // Import face profile
-const JSON_PROFILE = require('../descriptors/bnk48.json');
+const JSON_PROFILE = require("../descriptors/bnk48.json");
 
 // Initial State
 const INIT_STATE = {
-  imageURL: testImg,
+  imageURL: warnImg,
   fullDesc: null,
   detections: null,
   descriptors: null,
-  match: null
+  match: null,
+  noFacesDetected: false,
 };
 
 class ImageInput extends Component {
@@ -30,29 +32,30 @@ class ImageInput extends Component {
   };
 
   handleImage = async (image = this.state.imageURL) => {
-    await getFullFaceDescription(image).then(fullDesc => {
+    await getFullFaceDescription(image).then((fullDesc) => {
       if (!!fullDesc) {
         this.setState({
           fullDesc,
-          detections: fullDesc.map(fd => fd.detection),
-          descriptors: fullDesc.map(fd => fd.descriptor)
+          detections: fullDesc.map((fd) => fd.detection),
+          descriptors: fullDesc.map((fd) => fd.descriptor),
+          noFacesDetected: fullDesc.length === 0,
         });
       }
     });
 
     if (!!this.state.descriptors && !!this.state.faceMatcher) {
-      let match = await this.state.descriptors.map(descriptor =>
-        this.state.faceMatcher.findBestMatch(descriptor)
+      let match = await this.state.descriptors.map((descriptor) =>
+        this.state.faceMatcher.findBestMatch(descriptor),
       );
       this.setState({ match });
     }
   };
 
-  handleFileChange = async event => {
+  handleFileChange = async (event) => {
     this.resetState();
     await this.setState({
       imageURL: URL.createObjectURL(event.target.files[0]),
-      loading: true
+      loading: true,
     });
     this.handleImage();
   };
@@ -62,7 +65,7 @@ class ImageInput extends Component {
   };
 
   render() {
-    const { imageURL, detections, match } = this.state;
+    const { imageURL, detections, match, noFacesDetected } = this.state;
 
     let drawBox = null;
     if (!!detections) {
@@ -75,24 +78,24 @@ class ImageInput extends Component {
           <div key={i}>
             <div
               style={{
-                position: 'absolute',
-                border: 'solid',
-                borderColor: 'blue',
+                position: "absolute",
+                border: "solid",
+                borderColor: "blue",
                 height: _H,
                 width: _W,
-                transform: `translate(${_X}px,${_Y}px)`
+                transform: `translate(${_X}px,${_Y}px)`,
               }}
             >
               {!!match ? (
                 <p
                   style={{
-                    backgroundColor: 'blue',
-                    border: 'solid',
-                    borderColor: 'blue',
+                    backgroundColor: "blue",
+                    border: "solid",
+                    borderColor: "blue",
                     width: _W,
                     marginTop: 0,
-                    color: '#fff',
-                    transform: `translate(-3px,${_H}px)`
+                    color: "#fff",
+                    transform: `translate(-3px,${_H}px)`,
                   }}
                 >
                   {match[i]._label}
@@ -104,17 +107,30 @@ class ImageInput extends Component {
       });
     }
 
+    let detectionMessage;
+    if (noFacesDetected) {
+      detectionMessage = (
+        <p style={{ color: "red" }}>
+          No Faces Detected. Choose a picture with a face.
+        </p>
+      );
+    } else if (!!detections) {
+      detectionMessage = <p>Faces Detected: {detections.length}</p>;
+    }
+
     return (
       <div>
+        <Link to={"/"}>Home page</Link>
         <input
           id="myFileUpload"
           type="file"
           onChange={this.handleFileChange}
           accept=".jpg, .jpeg, .png"
         />
-        <div style={{ position: 'relative' }}>
-          <div style={{ position: 'absolute' }}>
-            <img src={imageURL} alt="imageURL" />
+        {detectionMessage}
+        <div style={{ position: "relative" }}>
+          <div style={{ position: "absolute" }}>
+            {noFacesDetected ? "" : <img src={imageURL} alt="imageURL" />}
           </div>
           {!!drawBox ? drawBox : null}
         </div>
